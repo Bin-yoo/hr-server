0b8b6dc996013af862955311c5a6ef1bee49d30a
Bin_yoo <mapconn@live.com>
2019-10-21 19:24:09 +0800

    idea热部署重新可用.后台添加pagehelper依赖.虽然添加了mybatis-generator,但我觉得有点蠢

--- hrserver/src/main/java/com/cn/ncvt/biz/EmployeeBiz.java
+++ hrserver/src/main/java/com/cn/ncvt/biz/EmployeeBiz.java
@@ -1,7 +1,18 @@
 package com.cn.ncvt.biz;
 
+import com.cn.ncvt.entity.Employee;
+import com.cn.ncvt.mapper.EmployeeMapper;
 import com.cn.ncvt.result.Result;
+import com.cn.ncvt.result.ResultFactory;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @version : V1.0
@@ -12,7 +23,30 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 public class EmployeeBiz {
-    public Result getAllEmployeeFile() {
-        return null;
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    public Result getAllEmployeeFile(int page, int limit) {
+        //紧跟着的第一个查询方法会被分页
+        PageHelper.startPage(page, limit);
+        List<Employee> fileList = employeeMapper.selectAllEmployee();
+
+        if (fileList != null){
+            //用PageInfo对结果进行包装,获取分页信息
+            PageInfo pageInfo = new PageInfo(fileList);
+
+            //包装map返回前端
+            Map map = new HashMap();
+            map.put("total", pageInfo.getTotal());
+            map.put("totalPage" , pageInfo.getPages());
+            map.put("page" , pageInfo.getPageNum());
+            map.put("list", fileList);
+
+            return ResultFactory.buildSuccessResult(map);
+        } else {
+            return ResultFactory.buildFailResult("获取失败");
+        }
+
     }
 }
---
+++ hrserver/src/main/java/com/cn/ncvt/mapper/EmployeeMapper.java
@@ -0,0 +1,18 @@
+package com.cn.ncvt.mapper;
+
+import com.cn.ncvt.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @version : V1.0
+ * @ClassName: EmployeeMapper
+ * @Description: TODO
+ * @Auther: Bin
+ * @Date: 2019/10/21 16:53
+ **/
+@Mapper
+public interface EmployeeMapper {
+    List<Employee> selectAllEmployee();
+}
