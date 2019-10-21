f9bd0f6072651b76c0898b4b0b9e93125aeafadf
Bin_yoo <mapconn@live.com>
2019-10-21 16:51:30 +0800

    更替以下错误的类

---
+++ hrserver/src/main/java/com/cn/ncvt/biz/EmployeeBiz.java
@@ -0,0 +1,18 @@
+package com.cn.ncvt.biz;
+
+import com.cn.ncvt.result.Result;
+import org.springframework.stereotype.Service;
+
+/**
+ * @version : V1.0
+ * @ClassName: EmployeeBiz
+ * @Description: TODO
+ * @Auther: Bin
+ * @Date: 2019/10/21 16:47
+ **/
+@Service
+public class EmployeeBiz {
+    public Result getAllEmployeeFile() {
+        return null;
+    }
+}
