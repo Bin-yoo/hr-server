128fcce647ce5cedf839439c00efe6d0322ba12d
miniceno <1941439196@qq.com>
2019-10-22 20:13:15 +0800

    增加了添加员工信息，修改员工信息，删除员工信息的接口

--- hrserver/src/main/java/com/cn/ncvt/controller/EmployeeController.java
+++ hrserver/src/main/java/com/cn/ncvt/controller/EmployeeController.java
@@ -1,11 +1,12 @@
 package com.cn.ncvt.controller;
 
 import com.cn.ncvt.biz.EmployeeBiz;
+import com.cn.ncvt.entity.Employee;
 import com.cn.ncvt.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @version : V1.0
@@ -16,14 +17,54 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/employee")
+@Api(tags = "员工资料")
 public class EmployeeController {
 
     @Autowired
     EmployeeBiz employeeBiz;
 
+    /**
+     *
+     * @param page
+     * @param limit
+     * @return
+     */
     @GetMapping("/allEmp")
+    @ApiOperation(value = "查看员工资料", notes = "返回全部员工资料")
     public Result allEmployeeFile(int page, int limit){
         return employeeBiz.getAllEmployeeFile(page, limit);
     }
 
+    /**
+     * 添加员工
+     * @param employee 员工资料
+     * @return
+     */
+    @PostMapping("/addEmp")
+    @ApiOperation(value = "添加员工资料", notes = "将员工资料添加进数据库")
+    public Result addEmployeeFile(@RequestBody Employee employee){
+        return employeeBiz.addEmployeeFile(employee);
+    }
+
+    /**
+     * 更新员工资料
+     * @param employee 员工资历
+     * @return
+     */
+    @PutMapping("/updateEmp")
+    @ApiOperation(value = "修改员工资料", notes = "修改员工资料")
+    public Result updateEmployeeFile(@RequestBody Employee employee){
+        return employeeBiz.updateEmployeeFile(employee);
+    }
+
+    /**
+     * 通过id删除员工资料
+     * @param id 员工id
+     * @return
+     */
+    @DeleteMapping("/deleteEmp/{id}")
+    @ApiOperation(value = "删除员工资料", notes = "删除员工资料")
+    public Result deleteByIdEmployeeFile(@PathVariable Integer id){
+        return employeeBiz.deleteByIdEmployeeFile(id);
+    }
 }
