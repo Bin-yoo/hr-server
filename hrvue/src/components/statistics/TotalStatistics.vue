ae5940f755570e261502bb926420d793d7127255
weixiaoshou <807218823@qq.com>
2019-10-30 17:07:50 +0800

    初步完成综合信息统计界面职位统计标签页的布局

--- hrvue/src/components/statistics/TotalStatistics.vue
+++ hrvue/src/components/statistics/TotalStatistics.vue
@@ -2,18 +2,19 @@
     <div>
         <Tabs type="card">
             <TabPane label="部门统计"><DeparStatistics></DeparStatistics></TabPane>
-            <TabPane label="职位统计">标签二的内容</TabPane>
-            <TabPane label="员工生日">标签三的内容</TabPane>
+            <TabPane label="职位统计"><PositionStatistics></PositionStatistics></TabPane>
         </Tabs>
     </div>
 </template>
 
 <script>
     import DeparStatistics from './totalStatistics/DeparStatistics.vue'
+    import PositionStatistics from './totalStatistics/PositionStatistics.vue'
 
     export default {
         components: {
             'DeparStatistics': DeparStatistics,
+            'PositionStatistics': PositionStatistics,
         },
         data() {
             return {
