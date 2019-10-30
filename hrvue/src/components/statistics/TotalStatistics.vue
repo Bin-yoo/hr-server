76564e0cd05e090991731e00b3ec357ac97566d3
weixiaoshou <807218823@qq.com>
2019-10-30 16:33:03 +0800

    初步完成综合信息统计界面部门统计标签页的布局

--- hrvue/src/components/statistics/TotalStatistics.vue
+++ hrvue/src/components/statistics/TotalStatistics.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    <h1>
-      综合信息统计
-    </h1>
-  </div>
+    <div>
+        <Tabs type="card">
+            <TabPane label="部门统计"><DeparStatistics></DeparStatistics></TabPane>
+            <TabPane label="职位统计">标签二的内容</TabPane>
+            <TabPane label="员工生日">标签三的内容</TabPane>
+        </Tabs>
+    </div>
 </template>
+
+<script>
+    import DeparStatistics from './totalStatistics/DeparStatistics.vue'
+
+    export default {
+        components: {
+            'DeparStatistics': DeparStatistics,
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+    }
+</script>
+
+<style scoped>
+    
+</style>
