46f248b39601dd8b0fd62633661555db938472db
miniceno <1941439196@qq.com>
2019-11-06 16:32:40 +0800

    修改了调薪记录的接口

--- hrserver/src/main/java/com/cn/ncvt/mapper/SalaryLogMapper.java
+++ hrserver/src/main/java/com/cn/ncvt/mapper/SalaryLogMapper.java
@@ -1,10 +1,11 @@
 package com.cn.ncvt.mapper;
 
 import com.cn.ncvt.entity.SalaryLog;
+import io.swagger.models.auth.In;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 @Mapper
 public interface SalaryLogMapper {
-    List<SalaryLog> selectByEid();
+    List<SalaryLog> selectByEid(Integer eid);
 }
