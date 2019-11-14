69e4e8e865889cd6b9a563f4257f0f2a5a00c9a1
Bin_yoo <mapconn@live.com>
2019-11-14 22:52:52 +0800

    添加用户管理的上传头像功能

--- hrserver/src/main/java/com/cn/ncvt/util/UploadUtil.java
+++ hrserver/src/main/java/com/cn/ncvt/util/UploadUtil.java
@@ -1,5 +1,6 @@
 package com.cn.ncvt.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,20 +12,21 @@ import java.util.Date;
 
 public class UploadUtil {
 
-    public String upload(@RequestParam("avatar") MultipartFile avatar, HttpServletRequest request) {
+    private String uploadDir = "C:/uploadFiles/";
 
+    public String upload(@RequestParam("picture") MultipartFile picture, String dir) {
 
         //获取文件在服务器的储存位置
-        String path = request.getSession().getServletContext().getRealPath("/upload");
+        String path = uploadDir+dir;
         File filePath = new File(path);
         System.out.println("文件的保存路径：" + path);
         if (!filePath.exists() && !filePath.isDirectory()) {
             System.out.println("目录不存在，创建目录:" + filePath);
-            filePath.mkdir();
+            filePath.mkdirs();
         }
 
         //获取原始文件名称(包含格式)
-        String originalFileName = avatar.getOriginalFilename();
+        String originalFileName = picture.getOriginalFilename();
         System.out.println("原始文件名称：" + originalFileName);
 
         //获取文件类型，以最后一个`.`为标识
@@ -43,10 +45,10 @@ public class UploadUtil {
         //在指定路径下创建一个文件
         File targetFile = new File(path, fileName);
         try {
-            avatar.transferTo(targetFile);
+            picture.transferTo(targetFile);
             System.out.println("上传成功");
             //将文件在服务器的存储路径返回
-            return "http://192.168.1.100:8080/BBS/upload/"+fileName;
+            return "http://111.230.141.100:8080/uploadFiles/"+dir+"/"+fileName;
         } catch (IOException e) {
             System.out.println("上传失败");
             e.printStackTrace();
